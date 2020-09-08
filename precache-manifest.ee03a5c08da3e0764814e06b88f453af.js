29b5b9c485f9e6e8d6e7640b0e8a0c19cc3d96b5
Yukishige Nakajo <nakajo@chaintope.com>
2020-09-09 01:08:42 +0900

    Updates

---
+++ precache-manifest.ee03a5c08da3e0764814e06b88f453af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca2a85fb1bd89cb46fcac10a470bfc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45cc20f054a137acb061",
+    "url": "/static/css/main.27f190fb.chunk.css"
+  },
+  {
+    "revision": "d1fd2a2f4d003c28284e",
+    "url": "/static/js/2.ecc7fde6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ecc7fde6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45cc20f054a137acb061",
+    "url": "/static/js/main.40381e9c.chunk.js"
+  },
+  {
+    "revision": "5f1aaa140361a42c4811",
+    "url": "/static/js/runtime-main.af7f8cb1.js"
+  }
+]);
